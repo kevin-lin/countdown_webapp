7ac59faa658b6a587f084d5ec714dee02663262b
Kevin Lin <whatakevin@gmail.com>
2013-12-13 23:16:17 -0600

    Make root path redirect to app/index.html

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -97,7 +97,8 @@ StaticServlet.prototype.handleRequest = function(req, res) {
     if (err)
       return self.sendMissing_(req, res, path);
     if (stat.isDirectory())
-      return self.sendDirectory_(req, res, path);
+      return self.sendRedirect_(req, res, 'app/index.html')
+      // return self.sendDirectory_(req, res, path);
     return self.sendFile_(req, res, path);
   });
 }
