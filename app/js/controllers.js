140dc086fa70ac6ad2325ed714409bd4fd8ee7bb
Kevin Lin <whatakevin@gmail.com>
2013-12-23 10:36:47 -0600

    Ensure color combination matches url or redirect to index.html

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('userCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-
+  controller('userCtrl', ['$scope', '$location', function($scope, $location) {
+    if( ('/' + CryptoJS.SHA1(getColorCode())) != $location.path() )
+      window.location = "index.html#/";
   }]);
